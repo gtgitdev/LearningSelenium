7debd5d8a8036324a10ae093f452ab0d24805a88
gtgitdev <g.turton@hotmail.com>
2019-07-22 05:52:54 -0400

    Add project files.

---
+++ LearningSelenium/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LearningSelenium
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
